e684b47c1da165ee6d39e7a856977acbad18abee
aiden.allman <aiden.allman@merlinsystems.co.uk>
2018-03-29 13:52:41 +0100

    Added all

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development' || env ==='test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else if (env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://aiden:Phoebe2020@ds061198.mlab.com:61198/heroku_znx3frcj'
-};
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
