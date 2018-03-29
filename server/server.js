9b75c0027fd7e3daa9faa9c45cd72a034eedf590
aiden.allman <aiden.allman@merlinsystems.co.uk>
2018-03-29 09:00:09 +0100

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -24,13 +25,13 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
   res.send({todos});
-  }), (e) => {
+}, (e) => {
     res.status(400).send(e);
-  }
+  });
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started up at  ${port}`);
 });
 
 module.exports = {app};
