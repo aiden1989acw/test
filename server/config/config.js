5616b72b0092733bce34369394e4efceb387bf20
aiden.allman <aiden.allman@merlinsystems.co.uk>
2018-03-29 13:02:42 +0100

    Changed mongo address

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
+if (env === 'development' || env ==='test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+} else if (env === 'production') {
+  process.env.MONGODB_URI = 'mongodb://aiden:Phoebe2020@ds061198.mlab.com:61198/heroku_znx3frcj'
+};
